4280e74423becb8ae31ae77bd03a641fd59b98f9
Alex <almoav@gmail.com>
2015-04-27 21:29:15 -0700

    remove enemy parameters add enemy row randomization add enemy offset randomization add enemy speed randomization
    made these attributes object properties

--- js/app.js
+++ js/app.js
@@ -1,20 +1,17 @@
 // Enemies our player must avoid
-var Enemy = function(row, offset) {
-    /*  param: row      | int [1-3]     | cobblestone tile 
-        row to birth enemy object
-
-        param: offset   |   int         | x-offset birth 
-        position
-    */
+var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-    this.x = offset;
-    this.y = (83 * row) -20;
+    this.row = Math.max(Math.round(Math.random() * 3), 1);
+    this.speed = (Math.random() + .5) * 200;
+    this.x = Math.random() * -1000;
+    this.y = (83 * this.row) -20;
 }
+
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -22,18 +19,15 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    
-    //console.log("update enemy");
-    //console.log(dt);
-    this.x += (200*dt);
-
-
+    this.x += (this.speed*dt);
 }
+
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
+
 
 // Now write your own player class
 // This class requires an update(), render() and
@@ -44,14 +38,17 @@ var Player = function() {
     this.y = (83 * 5) - 20;
 }
 
+
 Player.prototype.update = function(dt) {
     //clamp x between -2 and 402;
     //this.x = Math.min(Math.max(this.x, -2), 402);
 }
 
+
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);    
 }
+
 
 Player.prototype.handleInput = function(key) {
     //TODO: translate key presses into directional movement
@@ -64,18 +61,17 @@ Player.prototype.handleInput = function(key) {
     } else if (key === "down") {
         ((this.y === 395) ? null : this.y += 83);
     }        
-    
-
-    //Player.update(move);
 }
+
+
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 var player = new Player();
-var bug1 = new Enemy(2, -200);
-var bug2 = new Enemy(3, -400);
-var allEnemies = [bug1, bug2];
-
+var bug1 = new Enemy();
+var bug2 = new Enemy();
+var bug3 = new Enemy();
+var allEnemies = [bug1, bug2, bug3];
 
 
 // This listens for key presses and sends the keys to your
