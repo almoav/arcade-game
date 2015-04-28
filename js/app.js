d7f924f20a8751c3a9c8b333dad434d76f9596fc
Alex <almoav@gmail.com>
2015-04-27 19:48:52 -0700

    add player class and corresponding prototype methods

--- js/app.js
+++ js/app.js
@@ -28,8 +28,24 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.sprite = 'images/char-boy.png';
+    this.loc = 0;
+}
 
+Player.prototype.update = function(dt) {
+    //TODO: translate inputs to movement
+    this.loc + 0;
+}
 
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);    
+}
+
+Player.prototype.handleInput = function(key) {
+    //TODO: translate key presses into directional movement
+    Player.update(move);
+}
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
@@ -46,5 +62,5 @@ document.addEventListener('keyup', function(e) {
         40: 'down'
     };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+    Player.handleInput(allowedKeys[e.keyCode]);
 });
